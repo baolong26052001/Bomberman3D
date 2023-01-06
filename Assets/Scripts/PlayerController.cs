8f61d7ead4ec1d8aee0f800a928773a7bef24cc2
baolong26052001 <80016176+baolong26052001@users.noreply.github.com>
2023-01-06 14:40:30 +0700

    a

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float xMoveSpeed;
+    [SerializeField] float yMoveSpeed;
+    [SerializeField] float zMoveSpeed;
+
+    public KeyCode inputUp = KeyCode.W;
+    public KeyCode inputDown = KeyCode.S;
+    public KeyCode inputRight = KeyCode.D;
+    public KeyCode inputLeft = KeyCode.A;
+
+    void Start()
+    {
+        
+    }
+
+    void Update() 
+    {
+        Movement();      
+    }
+
+    private void Movement()
+    {
+        Vector3 newPosition = new Vector3();
+        if (Input.GetKey(inputUp))
+        {
+            newPosition = new Vector3(0f,0f, zMoveSpeed);
+        }
+        else if (Input.GetKey(inputDown))
+        {
+            newPosition = new Vector3(0f,0f, -zMoveSpeed);
+            
+        }
+        else if (Input.GetKey(inputRight))
+        {
+            newPosition = new Vector3(xMoveSpeed,0f,0f);
+            
+        }
+        else if (Input.GetKey(inputLeft))
+        {
+            newPosition = new Vector3(-xMoveSpeed,0f,0f);
+            
+        }
+
+        transform.position = transform.position + (newPosition * Time.deltaTime);
+    }
+
+    private void PlaceBomb()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            
+        }
+    }
+}
